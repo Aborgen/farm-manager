161cf969ed095aa846c2f2bff64e4f4f43e26822
Aborgen <alexanderborgen@gmail.com>
2021-02-25 14:15:42 -0700

    Move PlotClass outside of Plot component to simplify matters

--- src/components/Plot/Plot.tsx
+++ src/components/Plot/Plot.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import React, { useState } from 'react';
 
 import { makeFocusable, FocusableProps } from 'components/FocusableWrapper';
@@ -26,84 +25,76 @@ class PlotClass extends React.Component<PlotProps & FocusableProps, any> {
     };
   }
 
-function Plot(props: PlotProps) {
-  class FocusablePlot extends React.Component<PlotProps & FocusableProps> {
-    constructor(props) {
-      super(props);
-      this.state = {
-        rows: defaultRows,
-      };
+  rowMax() {
+    let count = 0;
+    switch (this.props.grade) {
+      case PlotGrade.Poor:
+        count = 3;
+        break;
+      case PlotGrade.Good:
+        count = 5;
+        break;
+      case PlotGrade.Great:
+        count = 7;
+        break;
+      case PlotGrade.Excellent:
+        count = 10;
+        break;
     }
 
-    rowMax() {
-      let count = 0;
-      switch (props.grade) {
-        case PlotGrade.Poor:
-          count = 3;
-          break;
-        case PlotGrade.Good:
-          count = 5;
-          break;
-        case PlotGrade.Great:
-          count = 7;
-          break;
-        case PlotGrade.Excellent:
-          count = 10;
-          break;
-      }
-
-      return count;
-    }
-
-    plowRow(crop: Crop) {
-      if (this.rowMax() === this.state.rows.length || this.rowMax() < this.state.rows.length) {
-        return;
-      }
-      else if (this.context.seeds[crop].count === 0) {
-        console.log(`Out of ${crop} seeds!`);
-        return;
-      }
-
-      const nextRow: RowProps = {
-        crop,
-        stage: GrowthStage.Planted,
-      };
-
-      this.context.decSeeds(crop);
-      this.setState(prevState => ({
-        rows: [nextRow]
-      }));
-    }
-
-    renderRows() {
-      let l = [];
-      for (let i = 0; i < this.rowMax(); ++i) {
-        let row;
-        if (i < this.state.rows.length) {
-          row = <Row key={ i } { ...this.state.rows[i] } />;
-        }
-        else {
-          row = <DefaultRow key={ i } />;
-        }
-
-        l.push(row);
-      }
-
-      return l;
-    }
-
-    render() {
-      return (
-        <section className={`plot${this.props.isFocused ? " focused" : ""}`}
-          onClick={ () => this.props.handleClick() }>
-          <span className="counter">{ `${this.state.rows.length}/${this.rowMax()}` }</span>
-          {
-            this.renderRows()
-          }
-        </section>
-      );
-    }
+    return count;
   }
+
+  plowRow(crop: Crop) {
+    if (this.rowMax() === this.state.rows.length || this.rowMax() < this.state.rows.length) {
+      return;
+    }
+    else if (this.context.seeds[crop].count === 0) {
+      console.log(`Out of ${crop} seeds!`);
+      return;
+    }
+
+    const nextRow: RowProps = {
+      crop,
+      stage: GrowthStage.Planted,
+    };
+
+    this.context.decSeeds(crop);
+    this.setState((prevState: any) => ({
+      rows: [...prevState.rows, nextRow]
+    }));
+  }
+
+  renderRows() {
+    let l = [];
+    for (let i = 0; i < this.rowMax(); ++i) {
+      let row;
+      if (i < this.state.rows.length) {
+        row = <Row key={ i } { ...this.state.rows[i] } />;
+      }
+      else {
+        row = <DefaultRow key={ i } />;
+      }
+
+      l.push(row);
+    }
+
+    return l;
+  }
+
+  render() {
+    return (
+      <section className={`plot${this.props.isFocused ? " focused" : ""}`}
+        onClick={ () => this.props.handleClick() }>
+        { this.props.isFocused && <PlowDialogue plowRow={ this.plowRow.bind(this) } /> }
+        <span className="counter">{ `${this.state.rows.length}/${this.rowMax()}` }</span>
+        {
+          this.renderRows()
+        }
+      </section>
+    );
+  }
+}
 
 PlotClass.contextType = FarmSupply;
 const FocusablePlot = makeFocusable(PlotClass);
