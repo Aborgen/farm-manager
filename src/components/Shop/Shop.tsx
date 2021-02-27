2b1079bee85d86fe7ebdc4e8a827f3c9be11917f
Aborgen <alexanderborgen@gmail.com>
2021-02-26 17:10:15 -0700

    Update Shop to use TabList component to keep track of which section of the shop the user wants to view

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,14 +1,58 @@
 import { useState } from 'react';
 
+import TabList, { TabMember } from 'components/TabList';
 import SeedStall from './internal/SeedStall';
 
+enum ShopSection {
+  Seeds,
+  TEMP,
+};
+
+const members: TabMember<ShopSection>[] = [
+  {
+    identifier: ShopSection.Seeds,
+    name: "Seeds",
+  },
+  {
+    identifier: ShopSection.TEMP,
+    name: "TEMP",
+  },
+];
+
 function Shop() {
-  const [ isShopping, setIsShopping ] = useState(false);
+  const [ currentSection, setCurrentSection ] = useState<ShopSection | null>(null);
+  function openSection(section: ShopSection) {
+    setCurrentSection(section);
+  }
+
+  function renderShopSection() {
+    let node;
+    switch (currentSection) {
+      case ShopSection.Seeds:
+        node = <SeedStall />;
+        break;
+      case ShopSection.TEMP:
+        node = <h2>THIS IS TEMPORARY</h2>;
+        break;
+      default:
+        return null;
+    }
+
+    return (
+      <>
+        <TabList members={ members } selected={ currentSection } handleClick={ openSection } />
+        { node }
+      </>
+    );
+  }
 
   return (
     <div className="shop">
-      { isShopping && <SeedStall /> }
-      <button onClick={ () => isShopping ? setIsShopping(false) : setIsShopping(true) }>{ isShopping ? "Exit shop" : "Enter shop" }</button>
+      { renderShopSection() }
+      <button
+        onClick={ () => currentSection === null ? setCurrentSection(ShopSection.Seeds) : setCurrentSection(null) }>
+        { currentSection === null ? "Enter shop" : "Exit shop" }
+      </button>
     </div>
   );
 }
