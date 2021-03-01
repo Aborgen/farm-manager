6ec077ffdacd3787b3d7db95c81059431bff4361
Aborgen <alexanderborgen@gmail.com>
2021-03-01 15:19:24 -0700

    Update SeedStall component so that the user is unable to purchase more than the maximum holding capacity of seeds

--- src/components/Shop/internal/SeedStall.tsx
+++ src/components/Shop/internal/SeedStall.tsx
@@ -4,7 +4,15 @@ import { Crop } from 'types/Crops';
 function SeedStall() {
   const { seeds } = useFarmSupplyContext();
   function buySeed(crop: Crop) {
+    if (seedsAtCapacity(crop)) {
+      return;
+    }
+
     seeds.incSeeds(crop);
+  }
+
+  function seedsAtCapacity(crop: Crop) {
+    return seeds.state[crop].count === seeds.state[crop].max;
   }
 
   return (
@@ -14,7 +22,11 @@ function SeedStall() {
         Object.entries(Crop).map(([k, v], key) => (
           <div key={ key } className="shop-item">
             { k } seeds
-            <button onClick={ () => buySeed(v) }>buy</button>
+            <button
+              disabled={ seedsAtCapacity(v) }
+              onClick={ () => buySeed(v) }>
+              { seedsAtCapacity(v) ? "at capacity" : "buy" }
+            </button>
           </div>
         ))
       }
