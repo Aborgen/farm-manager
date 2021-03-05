75615b3153618224feaaf42f47a9aa990b373af5
Aborgen <alexanderborgen@gmail.com>
2021-03-05 16:24:07 -0700

    Create EstablishmentWrapper, which can be applied to class components (it sets a ref)
    As of now, to be an Establishment means being able to assign farmhands to the component. An Establishment is also Focusable.

---
+++ src/wrappers/EstablishmentWrapper.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import { FarmhandByIdentifier, Farmhand } from 'context/FarmSupply/Farmhands';
+import { makeFocusable, FocusableProps } from './FocusableWrapper';
+import { applyWrapperWithForwardRef } from './general';
+
+interface EstablishmentProps extends FocusableProps {
+  farmhands: FarmhandByIdentifier,
+  assignFarmhand: Function,
+  atFarmhandCapacity: Function,
+};
+
+function EstablishmentWrapper(Component: React.ComponentType<any>) {
+  function Establishment(props: EstablishmentProps, ref: React.Ref<React.ReactElement>) {
+    const [ farmhands, setFarmhands ] = useState({});
+    const [ farmhandCount, setFarmhandCount ] = useState(0);
+    const farmhandCapacity = useState(3)[0];
+
+    function assignFarmhand(farmhand: Farmhand) {
+      if (atFarmhandCapacity()) {
+        return;
+      }
+
+      farmhand.assignment = ref as React.RefObject<React.ReactElement>;
+      setFarmhands({ ...farmhands, [farmhand.id]: farmhand });
+      setFarmhandCount(farmhandCount + 1);
+    }
+
+    function atFarmhandCapacity() {
+      return farmhandCount === farmhandCapacity;
+    }
+
+    return (
+      <Component { ...props }
+        ref={ ref }
+        farmhands={ farmhands }
+        atFarmhandCapacity={ atFarmhandCapacity }
+        assignFarmhand={ assignFarmhand } />
+    );
+  }
+  
+  const Foo = React.forwardRef<React.ReactElement, any>(Establishment);
+  return makeFocusable(Foo);
+}
+
+export type { EstablishmentProps };
+
+export function makeEstablishment(Component: React.ComponentType<any>) {
+  return applyWrapperWithForwardRef(EstablishmentWrapper, Component);
+};
+
+export default EstablishmentWrapper;
