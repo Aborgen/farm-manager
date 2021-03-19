b5c6288d32540d46fe38b1d9ae80325798345f78
Aborgen <alexanderborgen@gmail.com>
2021-03-19 14:38:05 -0700

    Add aria-selected to buttons in TabList to indicate which tab is currently selected

--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -15,6 +15,8 @@ function TabList(props: TabListProps<any>) {
       {
         props.members.map((member, key) => (
           <button key={ key } className={ `tab${ member.identifier === props.selected ? " selected-button" : "" }` }
+            tabIndex={ props.selected === null ? -1 : 0 }
+            aria-selected={ member.identifier === props.selected }
             disabled={ member.identifier === props.selected }
             onClick={ () => props.setSelected(member.identifier) }>{ member.name }</button>
         ))
