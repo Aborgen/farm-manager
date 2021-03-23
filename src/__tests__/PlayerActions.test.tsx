b65d16f33a0199a6d3f47c3c0cb90a3a15ad86c2
Aborgen <alexanderborgen@gmail.com>
2021-03-23 16:38:58 -0700

    Add tests for the PlayerActions context

---
+++ src/__tests__/PlayerActions.test.tsx
@@ -0,0 +1,36 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import { usePlayerActionsContext, PlayerActionsProvider } from 'context/PlayerActions';
+
+describe.only("Tests for the establishments section of FarmSupply", () => {
+  let playerActions;
+  beforeEach(() => {
+    playerActions = renderHook(() => usePlayerActionsContext(), {
+      wrapper: PlayerActionsProvider
+    }).result;
+  });
+
+  test("setFocus sets a react ref in state", () => {
+    const ref = { current: 1 };
+    act(() => {
+      playerActions.current.setFocus(ref);
+    });
+
+    expect(playerActions.current.state.focus).toBe(ref);
+  });
+
+  test("clearFocus sets focus to null", () => {
+    const ref = { current: 1 };
+    act(() => {
+      playerActions.current.setFocus(ref);
+    });
+
+    expect(playerActions.current.state.focus).toBe(ref);
+
+    act(() => {
+      playerActions.current.clearFocus();
+    });
+
+    expect(playerActions.current.state.focus).toBeNull();
+  });
+});
