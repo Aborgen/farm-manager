1e4157103b3d909bc37cb0b2b8aea0ad43377eb9
Aborgen <alexanderborgen@gmail.com>
2021-03-23 14:32:49 -0700

    Update TheUI component to display AssignmentPanel inside of a toggle TabList

--- src/components/TheUI/index.tsx
+++ src/components/TheUI/index.tsx
@@ -1,19 +1,56 @@
+import { useState } from 'react';
+
 import Shop from 'components/Shop';
 import { useFarmSupplyContext } from 'context/FarmSupply';
 import { usePlayerActionsContext } from 'context/PlayerActions';
+import TabList, { TabMember } from 'components/TabList';
 import AssignmentPanel from './internal/AssignmentPanel';
 
-export default function TheUI() {
+enum Menu {
+  Assignment,
+};
+
+const members: TabMember<Menu>[] = [
+  {
+    identifier: Menu.Assignment,
+    name: "Assign Farmhands",
+  },
+];
+
+function TheUI() {
   const { seeds, farmhands } = useFarmSupplyContext();
   const playerContext = usePlayerActionsContext();
+  const [ currentMenu, setCurrentMenu ] = useState<Menu | null>(null);
   function clearFocus() {
     playerContext.clearFocus();
   }
 
+  function openMenu(menu: Menu | null) {
+    setCurrentMenu(menu);
+  }
+
+  function renderMenu() {
+    let node;
+    switch (currentMenu) {
+      case Menu.Assignment:
+        node = <AssignmentPanel />
+        break;
+      default:
+        node = null;
+    }
+
+    return (
+      <>
+        <TabList members={ members } selected={ currentMenu } setSelected={ openMenu } tabsAreToggleButtons={ true } />
+        { node }
+      </>
+    );
+  }
+
   return (
-      <section className="ui">
+      <div className="ui">
         <Shop />
-        <ol className="seed-supply">
+        <ol className="ui-seed-supply">
           {
             Object.entries(seeds.state).map(([crop, info], key) => (
             <li key={ key }>
@@ -22,18 +59,24 @@ export default function TheUI() {
             ))
           }
         </ol>
-        <ol className="farmhands">
+        <ol className="ui-farmhands">
           {
             Object.entries(farmhands.state.demographics).map(([category, info], key) => (
-            <li key={ key }>
-              { `${category}s: ${info.count}` }
-            </li>
+              <li key={ key }>
+                { `${category}s: ${info.count}` }
+              </li>
             ))
           }
           { farmhands.state.unassigned.length > 0 && <li>unassigned: { farmhands.state.unassigned.length }</li> }
         </ol>
+        <div className="ui-menus">
+          <div className="menu-container menu-assignment">
+            { renderMenu() }
+          </div>
+        </div>
         { playerContext.state.focus && <button onClick={ () => clearFocus() }>clear focus</button> }
-        <AssignmentPanel />
-      </section>
+      </div>
   );
 }
+
+export default TheUI;
