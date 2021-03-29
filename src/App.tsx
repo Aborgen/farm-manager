ab77f171b3e41a6d6cba475cebe3f1c385ca8c9e
Aborgen <alexanderborgen@gmail.com>
2021-03-29 14:32:15 -0700

    Add styling to main App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { FarmSupplyProvider } from 'context/FarmSupply';
 import { PlayerActionsProvider } from 'context/PlayerActions';
 import TheUI from 'components/TheUI';
 import Field from 'components/Field';
+import styles from './App.module.css';
 
 function App() {
   return (
     <PlayerActionsProvider>
     <FarmSupplyProvider>
-      <TheUI />
-      <Field />
+      <div className={ styles.app }>
+        <TheUI />
+        <Field />
+      </div>
     </FarmSupplyProvider>
     </PlayerActionsProvider>
   );
