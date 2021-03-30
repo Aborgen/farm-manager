fd08b82f74b679080a10d8ae35e20840a5723063
Aborgen <alexanderborgen@gmail.com>
2021-03-30 14:29:05 -0700

    Extract portion of TheUI component into internal MenuTabs component

--- src/components/TheUI/index.tsx
+++ src/components/TheUI/index.tsx
@@ -1,73 +1,20 @@
-import { useState } from 'react';
-
-import Shop from 'components/Shop';
 import { usePlayerActionsContext } from 'context/PlayerActions';
-import TabList, { TabMember } from 'components/TabList';
-import AssignmentPanel from './internal/AssignmentPanel';
+import MenuTabs from './internal/MenuTabs';
 import SeedList from './internal/SeedList';
 import styles from './TheUI.module.css';
 
-enum Menu {
-  Assignment,
-  Shop,
-};
-
-const members: TabMember<Menu>[] = [
-  {
-    identifier: Menu.Assignment,
-    name: "Assign",
-  },
-  {
-    identifier: Menu.Shop,
-    name: "Shop",
-  },
-];
-
 function TheUI() {
   const playerContext = usePlayerActionsContext();
-  const [ currentMenu, setCurrentMenu ] = useState<Menu | null>(null);
   function clearFocus() {
     playerContext.clearFocus();
   }
 
-  function openMenu(menu: Menu | null) {
-    setCurrentMenu(menu);
-  }
-
-  function renderMenu() {
-    let node;
-    switch (currentMenu) {
-      case Menu.Assignment:
-        node = <AssignmentPanel />
-        break;
-      case Menu.Shop:
-        node = <Shop />
-        break;
-      default:
-        node = null;
-    }
-
-    return (
-      <>
-        <TabList
-          members={ members }
-          selected={ currentMenu }
-          setSelected={ openMenu }
-          tabsAreToggleButtons={ true }
-          style={ {buttonDefault: `${styles["menu-button"]} text-with-border--large`, buttonSelected: styles["menu-button--selected"]} } />
-        { node }
-      </>
-    );
-  }
-
   return (
-      <div className={ styles.ui }>
-        <div className={ styles["menu-container"] }>
-          { renderMenu() }
-        </div>
-        <SeedList />
-        { playerContext.state.focus && <button onClick={ () => clearFocus() }>clear focus</button> }
-      </div>
+    <div className={ styles.ui }>
+      <MenuTabs />
+      <SeedList />
+      { playerContext.state.focus && <button onClick={ () => clearFocus() }>clear focus</button> }
+    </div>
   );
 }
 
---
+++ src/components/TheUI/internal/MenuTabs/index.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+
+import Shop from 'components/Shop';
+import TabList, { TabMember } from 'components/TabList';
+import AssignmentPanel from './internal/AssignmentPanel';
+import styles from './MenuTabs.module.css';
+
+enum Menu {
+  Assignment,
+  Shop,
+};
+
+const members: TabMember<Menu>[] = [
+  {
+    identifier: Menu.Assignment,
+    name: "Assign",
+  },
+  {
+    identifier: Menu.Shop,
+    name: "Shop",
+  },
+];
+
+function MenuTabs() {
+  const [ currentMenu, setCurrentMenu ] = useState<Menu | null>(null);
+
+  function openMenu(menu: Menu | null) {
+    setCurrentMenu(menu);
+  }
+
+  function renderMenu() {
+    let node;
+    let style;
+    switch (currentMenu) {
+      case Menu.Assignment:
+        node = <AssignmentPanel />;
+        style = styles.assignment;
+        break;
+      case Menu.Shop:
+        node = <Shop />;
+        style = styles.shop;
+        break;
+      default:
+        node = null;
+    }
+
+    return (
+      <>
+        <TabList
+          members={ members }
+          selected={ currentMenu }
+          setSelected={ openMenu }
+          tabsAreToggleButtons={ true }
+          style={ {buttonDefault: `${styles["menu-button"]} cream_text-with-border--large`, buttonSelected: styles["menu-button--selected"]} } />
+        {
+          node !== null &&
+          <div className={ `${styles["menu-container"]} ${style}` }>
+            { node }
+          </div>
+        }
+      </>
+    );
+  }
+
+  return (
+    <div className={ styles["menu-tabs"] }>
+      { renderMenu() }
+    </div>
+  );
+}
+
+export default MenuTabs;
