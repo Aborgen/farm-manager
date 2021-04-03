b08b610b175ad9beb2e13cc4dc0c884494074401
Aborgen <alexanderborgen@gmail.com>
2021-04-02 18:19:56 -0700

    Move tests from dedicated test file for Shop into the test file for testing TheUI component
    In order to make my test suite follow Testing-Library's test philosophy a little more closely, I have decided to test the behavior of the Shop component where it is actually located, rather than in isolation.

--- src/__tests__/TheUI.test.tsx
+++ src/__tests__/TheUI.test.tsx
@@ -4,7 +4,7 @@ import { FarmSupplyProvider } from 'context/FarmSupply';
 import { PlayerActionsProvider } from 'context/PlayerActions';
 import TheUI from 'components/TheUI';
 
-describe("Tests for TheUI component", () => {
+describe("Tests for the assign feature of the ui", () => {
   beforeEach(() => {
     render(
       <PlayerActionsProvider>
@@ -27,5 +27,47 @@ describe("Tests for TheUI component", () => {
 
     screen.getByRole("button", { name: /assign/i }).click();
     expect(screen.queryByRole("combobox", { name: /assign/i })).toBeNull();
+  });
+});
+
+describe("Tests for the shop feature of the ui", () => {
+  beforeEach(() => {
+    render(
+      <PlayerActionsProvider>
+      <FarmSupplyProvider>
+        <TheUI />
+      </FarmSupplyProvider>
+      </PlayerActionsProvider>
+    );
+  });
+
+  test("Pressing shop menu tab opens the Shop", () => {
+    expect(screen.queryByRole("button", { name: /seed/i })).toBeNull();
+    screen.getByRole("button", { name: /shop/i }).click();
+    expect(screen.queryByRole("button", { name: /seed/i })).not.toBeNull();
+  });
+
+  test("Pressing shop menu tab while Shop is already open closes the Shop", () => {
+    screen.getByRole("button", { name: /shop/i }).click();
+    expect(screen.queryByRole("button", { name: /seed/i })).not.toBeNull();
+
+    screen.getByRole("button", { name: /shop/i }).click();
+    expect(screen.queryByRole("button", { name: /seed/i })).toBeNull();
+  });
+
+  test("When shop is open, pressing the seed section button causes the seed store to be available", () => {
+    screen.getByRole("button", { name: /shop/i }).click();
+    expect(screen.queryByRole("button", { name: /seed/i })).not.toBeNull();
+
+    screen.getByRole("button", { name: /seed/i }).click();
+    expect(screen.queryByRole("button", { name: /buy/i })).not.toBeNull();
+  });
+
+  test("When shop is open, pressing the job section button causes the job board to be available", () => {
+    screen.getByRole("button", { name: /shop/i }).click();
+    expect(screen.queryByRole("button", { name: /seed/i })).not.toBeNull();
+
+    screen.getByRole("button", { name: /job/i }).click();
+    expect(screen.queryByRole("button", { name: /hire/i })).not.toBeNull();
   });
 });
