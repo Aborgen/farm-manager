76925f0986a6e355cc31d34e16ad1b5caa5fc0b0
Aborgen <alexanderborgen@gmail.com>
2021-04-02 17:52:48 -0700

    Update ScrollableInterface component's width and height to be controlled by props

--- src/components/ScrollableInterface/index.tsx
+++ src/components/ScrollableInterface/index.tsx
@@ -4,11 +4,13 @@ import styles from './ScrollableInterface.module.css';
 
 interface ScrollableInterfaceProps {
   children: React.ReactNode,
+  width: string,
+  height: string,
 };
 
 function ScrollableInterface(props: ScrollableInterfaceProps) {
   return (
-    <div className={ styles["display-container"] }>
+    <div className={ styles["display-container"] } style={ {width: props.width, height: props.height} }>
       <div className={ styles["display"] }>
         { props.children }
       </div>
