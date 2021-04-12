cf10210bd36a8d6a5862f7514f5ec1fecb2bab9b
Aborgen <alexanderborgen@gmail.com>
2021-04-12 15:55:32 -0700

    Fix TransferMenu tests to take into account changes in component

--- src/__tests__/TransferMenu.test.tsx
+++ src/__tests__/TransferMenu.test.tsx
@@ -14,8 +14,10 @@ const inc = jest.fn();
 const dec = jest.fn();
 const reset = jest.fn();
 const props: TransferMenuProps<TestProps> = {
-  DisplayComponent: TestContainer,
+  DisplayComponent: <TestContainer />,
   available: [{id: 0}],
+  fireTransfer: false,
+  setFireTransfer: jest.fn(),
   commitTransfer: jest.fn(),
   transferCountInc: inc,
   transferCountDec: dec,
@@ -29,20 +31,20 @@ describe("Tests for TransferMenu component", () => {
 
   test("Clicking an element inside the available pane transfers it to the outbound pane", () => {
     expect(screen.getByRole("heading", {name: "Available"}).parentNode.querySelector("[data-testId=element]")).not.toBeNull();
-    expect(screen.getByRole("heading", {name: "Outbound"}).parentNode.querySelector("[data-testId=element]")).toBeNull();
+    expect(screen.getByRole("heading", {name: "Outgoing"}).parentNode.querySelector("[data-testId=element]")).toBeNull();
     screen.getByTestId("element").click();
     expect(screen.getByRole("heading", {name: "Available"}).parentNode.querySelector("[data-testId=element]")).toBeNull();
-    expect(screen.getByRole("heading", {name: "Outbound"}).parentNode.querySelector("[data-testId=element]")).not.toBeNull();
+    expect(screen.getByRole("heading", {name: "Outgoing"}).parentNode.querySelector("[data-testId=element]")).not.toBeNull();
     expect(inc.mock.calls.length).toEqual(1);
   });
 
   test("Clicking an element inside the outbound pane transfers it to the available pane", () => {
     screen.getByTestId("element").click();
 
-    expect(screen.getByRole("heading", {name: "Outbound"}).parentNode.querySelector("[data-testId=element]")).not.toBeNull();
+    expect(screen.getByRole("heading", {name: "Outgoing"}).parentNode.querySelector("[data-testId=element]")).not.toBeNull();
     expect(screen.getByRole("heading", {name: "Available"}).parentNode.querySelector("[data-testId=element]")).toBeNull();
     screen.getByTestId("element").click();
-    expect(screen.getByRole("heading", {name: "Outbound"}).parentNode.querySelector("[data-testId=element]")).toBeNull();
+    expect(screen.getByRole("heading", {name: "Outgoing"}).parentNode.querySelector("[data-testId=element]")).toBeNull();
     expect(screen.getByRole("heading", {name: "Available"}).parentNode.querySelector("[data-testId=element]")).not.toBeNull();
     expect(inc.mock.calls.length).toEqual(1);
     expect(dec.mock.calls.length).toEqual(1);
